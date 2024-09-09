e92614718b3d0e12385d25435052704797912c22
sceloprince <sceloprince749@gmail.com>
2024-09-09 19:49:31 +0200

    craeting a dashboard that lets users choose between keys and passwords.

--- client/digital-safe-dashboard/src/app/dashboard/dashboard.component.ts
+++ client/digital-safe-dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private router: Router) { }
+
+  viewItems(type: string) {
+    this.router.navigate([`/${type}`]);
+  }
 }
