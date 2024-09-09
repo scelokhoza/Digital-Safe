3a5850b3d9fba48ceb91d44fbec335470afa9bde
sceloprince <sceloprince749@gmail.com>
2024-09-09 18:47:51 +0200

    creating the database

---
+++ resources/sql/create_database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE safe_db;
+
+USE safe_db;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE data_store(
+
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    type ENUM('key', 'password'),
+    app_name VARCHAR(255) NOT NULL,
+    encrypted_value TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
